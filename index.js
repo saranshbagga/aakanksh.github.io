aed4dae46c3c39950e337dfde59dae5966612ec8
saranshbagga <76037795+saranshbagga@users.noreply.github.com>
2021-08-24 20:19:30 +0530

    aakanksha

---
+++ index.js
@@ -0,0 +1,32 @@
+function ppe(){
+var cart = document.getElementsByClassName("ppe")
+cart[0].innerHTML= "Added to Cart"
+}
+
+function sani(){
+var cart = document.getElementsByClassName("sani")
+cart[0].innerHTML= "Added to Cart"
+}
+function mask(){
+var cart = document.getElementsByClassName("mask")
+cart[0].innerHTML= "Added to Cart"
+}
+function oats(){
+var cart = document.getElementsByClassName("oats")
+cart[0].innerHTML= "Added to Cart"
+}
+function biscuits(){
+var cart = document.getElementsByClassName("biscuits")
+cart[0].innerHTML= "Added to Cart"
+}
+function almonds(){
+var cart = document.getElementsByClassName("almonds")
+cart[0].innerHTML= "Added to Cart"
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
